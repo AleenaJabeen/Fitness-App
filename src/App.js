b2a86d276189b2d21b6b5a2c626054533b221420
AleenaJabeen <aleenajabeen19648@gmail.com>
2024-11-04 22:39:11 +0500

    changes made

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import Signup from './components/SignupComponent/Signup';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from './components/LoginComponent/Login';
-import Home from './components/HomeComponent/Home'
+import Home from './components/HomeComponent/Home';
+import './index.css';
 function App() {
   return (
     <>
