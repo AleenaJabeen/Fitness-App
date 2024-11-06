e3d9b580a04aeb36e43f7a70e2d594bc9774cf28
AleenaJabeen <aleenajabeen19648@gmail.com>
2024-11-06 09:08:55 +0500

    Footer component added

--- src/components/HomeComponent/Card.jsx
+++ src/components/HomeComponent/Card.jsx
@@ -6,6 +6,7 @@ function Card(props) {
       <section className="card">
         <img src="" alt="" />
         <h4>{props.title}</h4>
+        <button>Read More</button>
       </section>
     </>
   )
---
+++ src/components/HomeComponent/Purpose.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+function Purpose() {
+  return (
+    <>
+    <section className="purpose">
+        <h2>Purpose</h2>
+        <div className="purposeDetail">
+            <img src="" alt="" />
+            <div className="detail">
+                <p>It is designed to empower users in achieving
+their health and fitness goals through
+comprehensive tracking and personalized guidance.</p>
+<p>By integrating fitness tracking, nutrition
+management, and progress monitoring, the
+it allows users to seamlessly maintain a healthy
+lifestyle. The inclusion of community features
+and personalized coaching provides users with
+the support and motivation they need to achieve
+their fitness goals</p>
+<p>The Fitness Website is designed for a wide range of users</p>
+<table>
+    <tr>
+        <td>Fitness Enthusiasts</td>
+        <td>Beginners</td>
+    </tr>
+    <tr>
+        <td>Busy Professionals</td>
+        <td>Individuals</td>
+    </tr>
+</table>
+            </div>
+        </div>
+    </section>
+    </>
+  )
+}
+
+export default Purpose
