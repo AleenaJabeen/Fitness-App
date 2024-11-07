c198a79c482ed6ce56121ec9700bb4645bc70c40
AleenaJabeen <aleenajabeen19648@gmail.com>
2024-11-07 13:53:28 +0500

    g

--- src/components/LayoutComponents/Navbar.jsx
+++ src/components/LayoutComponents/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import styles from '../../styles/LayoutStyles/Navbar.module.css';
-import login from '../../assets/Layoutassets/user-profile-02.png';
+import login from '../../assets/Layoutassets/userprofile.png';
 
 function Navbar() {
   return (
